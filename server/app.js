471dc9bef220a531dc2ba6c86e6438ffcbb2ed32
aman <bora.amanalambora@gmail.com>
2020-01-05 14:21:33 +0530

    firebase-create

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var firebase = require("firebase");
 var fireadmin = require("firebase-admin");
 
 var app = express();
@@ -11,12 +12,28 @@ app.listen(3201, ()=>{
   console.log(`Listening at port 3201`);
 });
 
-app.get("/", (req, res) => {
-    res.send('hello');
-});
 
 var serviceAccount = require("./serviceAccountKey.json");
 fireadmin.initializeApp({
   credential : fireadmin.credential.cert(serviceAccount),
   databaseUrl : "http://sih2020-check.firebase.com"
+});
+
+
+
+app.get("/", (req, res) => {
+  res.send('hello');
+});
+
+
+app.get("/createUser",(req, res) => {
+  var username = req.body.name;
+  var phn = req.body.phone_number;
+
+  var data = {
+    'name': username,
+    'phn' : phn
+  };
+
+  firebase.database.ref().child('user').push(data);
 });
