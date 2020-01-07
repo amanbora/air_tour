45de07f332015db10f2641671939830a24464bd8
avinash0705 <avinash7may@gmail.com>
2020-01-07 10:16:32 +0530

    added flash component and our centers page

---
+++ src/app/pages/flash-screen/flash-screen.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import {MatGridListModule} from '@angular/material/grid-list';
+
+
+@Component({
+  selector: 'app-flash-screen',
+  templateUrl: './flash-screen.component.html',
+  styleUrls: ['./flash-screen.component.css']
+})
+export class FlashScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
