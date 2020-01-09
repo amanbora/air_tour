452fb3cf2e6bc10eb5eced7e2f924b3485e0d598
avinash0705 <avinash7may@gmail.com>
2020-01-09 18:36:04 +0530

    edit profile

---
+++ src/app/pages/edit-profile/edit-profile.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {UserProfile} from '../../models/UserProfile'
+
+
+@Component({
+  selector: 'app-edit-profile',
+  templateUrl: './edit-profile.component.html',
+  styleUrls: ['./edit-profile.component.css']
+})
+export class EditProfileComponent implements OnInit {
+
+  constructor() { }
+  
+
+  ngOnInit() {
+  }
+
+}
