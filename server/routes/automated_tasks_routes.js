65e43c1894881d8cab5a5d3f8c704065ed270fe4
its-the-Ayush-kumar <its.the.ayush.kumar@gmail.com>
2020-01-10 22:54:37 +0530

    api's working

--- server/routes/automated_tasks_routes.js
+++ server/routes/automated_tasks_routes.js
@@ -5,6 +5,6 @@ var firebase = require("firebase");
 //1. check every moment which journey is about to start
 //2. assign porters and cab drivers to every journey 1/2 hour before its start
 //3. sending notifications
-//4. 
+//4. ...
 
 module.exports = router;
