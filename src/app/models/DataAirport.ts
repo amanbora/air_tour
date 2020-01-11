893f371a00d0bf2ce408dde948865f3b96f80e1b
aman <bora.amanalambora@gmail.com>
2020-01-11 19:31:18 +0530

    airport

---
+++ src/app/models/DataAirport.ts
@@ -0,0 +1,28 @@
+
+export interface DataAirport {
+    id: string;
+    name: string;
+  }
+
+
+  /** list of DataAirports */
+export const Airports: DataAirport[] = [
+    {name: 'DataAirport A (Switzerland)', id: 'A'},
+    {name: 'DataAirport B (Switzerland)', id: 'B'},
+    {name: 'DataAirport C (France)', id: 'C'},
+    {name: 'DataAirport D (France)', id: 'D'},
+    {name: 'DataAirport E (France)', id: 'E'},
+    {name: 'DataAirport F (Italy)', id: 'F'},
+    {name: 'DataAirport G (Italy)', id: 'G'},
+    {name: 'DataAirport H (Italy)', id: 'H'},
+    {name: 'DataAirport I (Italy)', id: 'I'},
+    {name: 'DataAirport J (Italy)', id: 'J'},
+    {name: 'DataAirport Kolombia (United States of America)', id: 'K'},
+    {name: 'DataAirport L (Germany)', id: 'L'},
+    {name: 'DataAirport M (Germany)', id: 'M'},
+    {name: 'DataAirport N (Germany)', id: 'N'},
+    {name: 'DataAirport O (Germany)', id: 'O'},
+    {name: 'DataAirport P (Germany)', id: 'P'},
+    {name: 'DataAirport Q (Germany)', id: 'Q'},
+    {name: 'DataAirport R (Germany)', id: 'R'}
+  ];
