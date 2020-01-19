03d81c66a0ca3c8dcfab62d068e8397bcb0f5944
aman <bora.amanalambora@gmail.com>
2020-01-19 15:55:29 +0530

    official

--- src/app/ml-model/data/data.component.ts
+++ src/app/ml-model/data/data.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+interface ima{
+  url : string,
+  name : string
+}
+
 @Component({
   selector: 'app-data',
   templateUrl: './data.component.html',
@@ -8,8 +13,14 @@ import { Component, OnInit } from '@angular/core';
 export class DataComponent implements OnInit {
 
   constructor() { }
-
+  imageUrl: ima[] = [];
   ngOnInit() {
+    this.imageUrl.push( { 'url' :'./../../../assets/images/img-data/Daily_trends.png', 'name': 'Daily Trends'});
+    this.imageUrl.push({ 'url': './../../../assets/images/img-data/Monthly_trends.png', 'name': 'Monthly Trends'});
+    this.imageUrl.push({ 'url': './../../../assets/images/img-data/Weekends_trend.png', 'name': 'Weekly Trends'});
+    this.imageUrl.push({ 'url': './../../../assets/images/img-data/Population_growth.png', 'name': 'Poplution Growth'});
+
+    console.log(this.imageUrl[0].url, this.imageUrl[0].name)
   }
 
 }
--- src/app/ml-model/machine/machine.component.ts
+++ src/app/ml-model/machine/machine.component.ts
@@ -1,16 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { MachineService } from 'src/app/services/machine.service';
 
+
+interface ima{
+  url : string,
+  name : string
+}
+
+
 @Component({
   selector: 'app-machine',
   templateUrl: './machine.component.html',
   styleUrls: ['./machine.component.css']
 })
+
 export class MachineComponent implements OnInit {
   mlData: any;
   constructor(private machine: MachineService) { }
 
+  imageUrl: ima[] = [];
+
   ngOnInit() {
+
+
+    this.imageUrl.push( {'url' : './../../../assets/images/img-data/BahíaCentroamericaRestaurant.jpg' , 'name': 'Bahía Centroamerica'});
+  
+    this.imageUrl.push( { 'url' :'./../../../assets/images/img-data/battiste-cajun-1.png', 'name': 'battiste-cajun-1'});
+    this.imageUrl.push({ 'url': './../../../assets/images/img-data/burger-king-restaurant-interior-bangkok-thailand-june-often-abbreviated-as-bk-global-chain-hamburger-fast-food-64599797.jpg', 'name': 'Burger-king'});
+
   }
 
   funcHello() {
